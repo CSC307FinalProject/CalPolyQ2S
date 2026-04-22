9fdda0af0a399fe3f2383486ce8015ef3047a42b
yosem <yosemitebone@gmail.com>
2026-04-22 11:16:01 -0700

    Added email and password lines

--- packages/frontend/src/pages/login.tsx
+++ packages/frontend/src/pages/login.tsx
@@ -4,6 +4,10 @@ import react from "react"
 export default function Login() {
     return (
         <>
+        <p>Email address</p>
+        
+        <p>Password</p>
+
         <h1>LOGIN PAGE: Hello World</h1>
         </>
     )
