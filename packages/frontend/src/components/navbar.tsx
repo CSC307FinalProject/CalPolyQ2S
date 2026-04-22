92686fb3b249ab19cb0863d32487ba35de2810b3
camdnn <camden.mann0105@gmail.com>
2026-04-22 11:09:52 -0700

    added tailwind to system

---
+++ packages/frontend/src/components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function homepageNavbar() {
+  return (
+    <div className="w-full h-20 bg-white">
+      <p>Calpoly Q2S</p>
+    </div>
+  );
+}
--- packages/frontend/src/pages/homepage.tsx
+++ packages/frontend/src/pages/homepage.tsx
@@ -1,11 +1,12 @@
-import react from "react"
-
+import Navbar from "../components/navbar";
 
 export default function Homepage() {
-    return (
-        <>
-        <h1>Hello World</h1>
-        </>
-    )
-
+  return (
+    <div className="pt-5 pl-10 bg-black">
+      <Navbar />
+      <h1 className="bg-white/30 backdrop-blur-md w-full">
+        Confused on the Quarter to Semester Change?
+      </h1>
+    </div>
+  );
 }
