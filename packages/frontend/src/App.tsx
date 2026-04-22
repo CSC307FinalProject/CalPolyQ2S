bdc7a6712a3933403a044035a66cc4cbd9c73f88
Hayk <hayk.chaloyan@gmail.com>
2026-04-22 10:53:00 -0700

    Added login routing to App.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Homepage from './pages/homepage';
+import Login from "./pages/login";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   )
