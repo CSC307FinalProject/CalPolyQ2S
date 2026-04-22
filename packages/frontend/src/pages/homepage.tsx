bca0e71c1286429a7b5767c383eab4342fa622ea
JBruzer <jbruzgul@calpoly.edu>
2026-04-22 11:07:20 -0700

    Get Started Button on Homepage

--- packages/frontend/src/pages/homepage.tsx
+++ packages/frontend/src/pages/homepage.tsx
@@ -1,11 +1,14 @@
-import react from "react"
+import "./homepage.css";
 
-
-export default function Homepage() {
-    return (
-        <>
-        <h1>Hello World</h1>
-        </>
-    )
-
+function Homepage() {
+  return (
+    <main className="home">
+      <h1>Welcome</h1>
+      <button className="btn-primary" onClick={() => console.log("clicked")}>
+        Get Started
+      </button>
+    </main>
+  );
 }
+
+export default Homepage;
