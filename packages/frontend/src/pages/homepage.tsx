33a138f7e512b9b5a7ba14fa934cc7ddcd19361c
JBruzer <jbruzgul@calpoly.edu>
2026-04-29 10:34:16 -0700

    Finishing Touches

--- packages/frontend/src/pages/homepage.tsx
+++ packages/frontend/src/pages/homepage.tsx
@@ -20,20 +20,31 @@ function Homepage() {
           <Navbar />
         </div>
 
-        <div className="text-left bg-[#D7F5FF] text-[#047494] text-xs font-bold flex justify-baseline p-1.5 mt-10 rounded-xl w-fit">
+        <div className="text-left bg-[#D7F5FF] text-[#047494] text-xs font-bold flex justify-baseline p-1.5 mt-16 mb-2 rounded-xl w-fit">
           CAL POLY SLO — QUARTER TO SEMESTER
         </div>
 
-        <div className="text-left leading-tight flex justify-baseline text-white font-bold text-5xl w-[50vw]">
+        <div className="text-left leading-tight flex justify-baseline text-white font-bold text-6xl w-[50vw]">
           Confused on the Quarter to Semester Change?
         </div>
-        <div className="text-left text-white px-3 py-8 bg-black/5 backdrop-blur-md rounded-xl w-[50vw]">
+        <div className="text-left text-white text-xl px-3 py-8 bg-black/5 backdrop-blur-md rounded-xl w-[55vw] mt-5">
           Starting 2026, Cal Poly SLO is switching from the quarter system to
           the semester system. Compare timelines, reduce your course load, and
           see how classes connect—all in one place to choose the catalog that is
           best.
         </div>
-        <button className="btn-primary text-left leading-tight flex justify-baseline absolute bottom">
+        <button
+          className="
+    text-left text-black bg-white
+    px-6 py-4 rounded-2xl
+    text-lg font-semibold leading-tight
+    flex items-center justify-start mt-5
+    transition-all duration-150 ease-out
+    hover:scale-105 hover:bg-gray-50
+    active:scale-95 active:bg-gray-100
+    cursor-pointer select-none
+  "
+        >
           Get Started
         </button>
       </div>
