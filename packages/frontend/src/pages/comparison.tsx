468464e7a0c666f7c81e2676140e21e398ea0ae6
Tyler Pike <yosemitebone@gmail.com>
2026-05-04 18:46:58 -0700

    Created Units and Terms cards

--- packages/frontend/src/pages/comparison.tsx
+++ packages/frontend/src/pages/comparison.tsx
@@ -1,12 +1,11 @@
 import react from "react"
-import UnitsBox from "../components/UnitsCompleted"
-import UnitsCard from "../components/UnitsCompleted"
+import {Cards} from "../components/UnitsCompleted"
 
 export default function Comparison() {
 
     return (
         <>
-        <UnitsCard units={95} unit_type="Quarter"></UnitsCard>
+        <Cards units={95} terms={5} unit_type="Quarters"></Cards>
         <h1>Hello World from the comparison page</h1>
         
         </>
