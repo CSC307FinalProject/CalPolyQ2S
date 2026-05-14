d99ad50074ec0f722b3495db9d5c727facd37db4
Hayk <hayk.chaloyan@gmail.com>
2026-05-13 22:58:50 -0700

    Added authenticateUser route

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -2,7 +2,7 @@ import sql from "./db/index.js"
 import cors from "cors";
 import express from "express";
 import "dotenv/config";
-import { loginUser, registerUser } from "./auth.js";
+import { authenticateUser, loginUser, registerUser } from "./auth.js";
 
 
 const app = express();
@@ -14,6 +14,12 @@ app.use(express.json());
 app.post("/login", loginUser);
 app.post("/register", registerUser);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.post("/users", authenticateUser, async (req, res) => {
+  const {email} = req.body;
+  const [user] = await sql`INSERT INTO students (email) VALUES (${email}) RETURNING *`;
+  res.status(201).send(user);
+});
 
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
