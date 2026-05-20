992af8aef7b5e7036908676d35261feb4bac09ba
CurrenSebastian <currensebastian@gmail.com>
2026-05-20 12:29:11 -0700

    New fixes in register Form

--- packages/frontend/src/components/registerForm.tsx
+++ packages/frontend/src/components/registerForm.tsx
@@ -34,7 +34,7 @@ export default function RegisterForm() {
     event.preventDefault();
 
     try {
-      const response = await fetch(("/register"), {
+      const response = await fetch(apiUrl("/register"), {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
