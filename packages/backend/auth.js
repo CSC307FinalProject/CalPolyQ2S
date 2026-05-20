5d12eef16207f0f82de9e50dd6133eed87d9ce16
Hayk <hayk.chaloyan@gmail.com>
2026-05-20 10:34:42 -0700

    Added jwt import

--- packages/backend/auth.js
+++ packages/backend/auth.js
@@ -3,7 +3,7 @@ import sql from "./index.js";
 import cors from "cors";
 import express from "express";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = 3000;
