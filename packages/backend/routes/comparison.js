d77ee92f539c7c4718a826f0fdf991a6c02605fb
CurrenSebastian <currensebastian@gmail.com>
2026-05-26 21:56:02 -0700

    Ran Prettier

--- packages/backend/routes/comparison.js
+++ packages/backend/routes/comparison.js
@@ -68,7 +68,7 @@ router.get("/:student_id", async (req, res) => {
   WHERE sc.student_id = ${student_id}
 `;
 
-const quarterCourses = await sql`
+    const quarterCourses = await sql`
   SELECT
     course_id,
     subject || ' ' || course_number AS course_code,
@@ -78,7 +78,7 @@ const quarterCourses = await sql`
   WHERE catalog_id = 1
 `;
 
-const semesterCourses = await sql`
+    const semesterCourses = await sql`
   SELECT
     course_id,
     subject || ' ' || course_number AS course_code,
