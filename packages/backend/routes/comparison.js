d6c3d3f96b729412c6d907695e6a7ba5111bda3d
Tyler Pike <yosemitebone@gmail.com>
2026-06-03 14:09:19 -0700

    Updated SQL queries to filter for concentration

--- packages/backend/routes/comparison.js
+++ packages/backend/routes/comparison.js
@@ -138,6 +138,7 @@ from requirement_groups as rg
 join requirement_group_courses as rgc on rg.group_id = rgc.group_id
 join taken_quarter_courses as tqc on tqc.course_id = rgc.course_id
 join courses as c on c.course_id = rgc.course_id
+join students as s on s.student_id = 31 and (rg.concentration_id is null or rg.concentration_id = s.concentration_id)
 where
 rg.catalog_id = ${catalog_id})
 select sum(units) as units
@@ -213,6 +214,7 @@ async function queryNeededClasses(student_id, courseMap) {
       join requirement_groups as groups on groups.group_id = gc.group_id
       left join taken_quarter_courses as tcs on tcs.course_id = gc.course_id
       left join courses on courses.course_id = tcs.course_id
+      join students as s on s.student_id = 31 and (groups.concentration_id is null or groups.concentration_id = s.concentration_id)
     group by
       groups.group_id,
       groups.catalog_id,
